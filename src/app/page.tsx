86620552c4e6afb3d470e5d169f1bfd35faf360f
bruna.kobayachi <brunakobayachi@gmail.com>
2023-06-24 16:28:23 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+	return (
+		<div>
+			<div>CARA, TIPO ASSIM</div>
+      <img src="https://d38ty5v1e1ccso.cloudfront.net/63fd0f2067e29a205bbf1015/files/8aff4db4-0f67-4568-9a2e-213b6a6ec767.png" alt=""/>
+		</div>
+	)
+}
